a9d79b0adbb199cbb830125ea46b84a0922adee4
mnagi <mnagi@youxel.com>
2025-01-03 14:03:06 +0200

    Initial Commit

---
+++ TestingPod/TestingPod.h
@@ -0,0 +1,18 @@
+//
+//  TestingPod.h
+//  TestingPod
+//
+//  Created by Mohamed Nagi on 03/01/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestingPod.
+FOUNDATION_EXPORT double TestingPodVersionNumber;
+
+//! Project version string for TestingPod.
+FOUNDATION_EXPORT const unsigned char TestingPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestingPod/PublicHeader.h>
+
+
