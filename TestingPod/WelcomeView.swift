1f9cea4b1fcac74fea7f039a38bc65d08470b69c
mnagi <mnagi@youxel.com>
2025-01-03 14:06:28 +0200

    Added WelcomeView file

---
+++ TestingPod/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  TestingPod
+//
+//  Created by Mohamed Nagi on 03/01/2025.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text("Hello, World! \n this is my welcome view inside my TestingPod")
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
